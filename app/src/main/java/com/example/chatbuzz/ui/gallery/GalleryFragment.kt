5d33da241945ba078b42233c5ccb8c3094592a9e
pranav-bharadwaj <pranavbharadwaj2001@gmail.com>
2021-08-30 02:29:36 +0530

    first app

---
+++ app/src/main/java/com/example/chatbuzz/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.chatbuzz.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.chatbuzz.R
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_signup, container, false)
+        val textView: TextView = root.findViewById(R.id.text_login)
+        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
